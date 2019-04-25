a5eed3be98a30270f1868def9cc9cf5669512884
Trevor YH Ho <trevor.ho@ed.ac.uk>
2019-04-25 22:10:02 +0100

    PR script updated

--- IBM_PRDataProcessing.py
+++ IBM_PRDataProcessing.py
@@ -9,97 +9,87 @@ that contains information on corrected OD600, corrected red fluorescence
 and corrected fluorescence/OD600, it also adds metadata to the file regarding
 the Run number, the SampleID and the condition
 
-This script is tailored for FC014 Run 1
+This script is tailored for FC014 Run 2
 
-It should be noted that this way of processing is not sustainable because the
-conditions of the plates will only get more and more complex. Needs to rework
-the code to generate something that automatically match plate number and 
-plate metadata file.
 """
 
 import os
 import pandas as pd
-
-# Function for renaming Index
-def renameIndex(indexStr=str):
-    indexStr = indexStr[0] + str(int(indexStr[1:3]))
-    return indexStr
+import IBM_CustomFunctions as cf
 
 # TODO: Specify folder location
-# TODO: Prepare file of metadata
 dataRootDir=r'W:\Data storage & Projects\PhD Project_Trevor Ho\3_Intein-assisted Bisection Mapping'
 dataFolderDir='FC014'
-metafilename = 'IBM_FC014R1_PRPlateMetadata.xlsx'
-outputCSV = 'IBM_FC014R1_PRData.csv'
 
-
-#%% Since there is only a single metafile in this experiment
+# TODO: Prepare file of metadata
+# Check that the blank well has been labeled as "Blank"
+metafilename = 'PRMD_IBM_FC014R2.xlsx'
+outputCSV = 'IBM_FC014R2-4_PRData.csv'
 
 # Read PR Metadata Excelfile
 metadataDir = os.path.join(dataRootDir,dataFolderDir,metafilename)
-metaxls= pd.ExcelFile(metadataDir)
-metadata = {sheet:metaxls.parse(sheet) for sheet in metaxls.sheet_names}
-
-# Process the df of metadata & replace the old dictionary with new metadata dataframes
-for meta_property, metadf_96format in metadata.items():
-    metadf = pd.DataFrame(columns = [])
-    for char in ['A','B','C','D','E','F','G','H']:
-        metadf_96_row = metadf_96format.transpose()[char]
-        metadf_96_row = metadf_96_row.to_frame(meta_property)
-        indexList = metadf_96_row.index.tolist()
-        
-        # Retrieve information of well and set it as index
-        for columnIndex in range(len(indexList)):
-            indexList[columnIndex] = char + str(indexList[columnIndex])
-        metadf_96_row['Well']=indexList
-        metadf_96_row.set_index('Well', inplace=True)
-        
-        # Append to major dataframe of the single metadata
-        metadf = metadf.append(metadf_96_row,sort=False)
-    metadata[meta_property] = metadf
+metadata_df = cf.metadata_to_metadf(metadataDir)
 
 #%%
 
 alldata = pd.DataFrame(columns = [])
 
 folderDir = os.path.join(dataRootDir, dataFolderDir)
-# This script loops through all PR data automatically instead of mapping each file with a "blank finder"
+
+'''
+Strategy: 
+    Loop through every single file starting with 'PR', check if filename contains the key
+    If yes, process it and map the metadata onto it
+
+'''
+
+keylist = [
+        'FC014R2',
+        'FC014R3',
+        'FC014R4'
+           ]
+
 for file in os.listdir(folderDir):
      filename = os.fsdecode(file)
-     if filename.endswith(".xlsx") and filename[0:2] == 'PR':
+
+     if filename.endswith(".xlsx") and filename.split('_')[0] == 'PR' \
+     and (not keylist or any(key in filename for key in keylist)):
          
-         filenameParts_info = filename.split('_')[3]
-         run_no = filenameParts_info[1]
-         ind_time = filenameParts_info.split('PI')[1].split('P')[0]
-         ind_cond_code = filenameParts_info.split('PI')[1].split('P')[1][0]
+         # Read Excel file
          excelDir = os.path.join(folderDir,filename)
-         data=pd.read_excel(excelDir,sheet_name = 'End point',index_col=0,skiprows=12)
+         data = pd.read_excel(excelDir,sheet_name = 'End point',index_col=0,skiprows=12)
          data = data.drop(['Content','Raw Data (600 1)','Raw Data (584 2)'], axis=1)
          
          # Update the index so it matches the conventional nomenclature
-         indexList = data.index.tolist()
-         for n, ind in enumerate(indexList):
-             indexList[n]= renameIndex(ind)
-         data.set_index(pd.Index(indexList),inplace=True)
+         data = cf.renameDfWellIndex(data)
+         
+         # Extract information from filename
+         filename = filename.split('.xlsx')[0][3:]    #Removes 'PR' from filename for downstream analysis
+         
+         run_no = int(filename.split('R')[1][0])
+                  
+         # Check if induction time and plate no info are in the filename
+         if filename.find('PI')>=0:
+             ind_time_frag = filename.split('PI')[1]
+             if ind_time_frag.find('P') >=0:
+                 ind_time, plate_no = int(ind_time_frag.split('P')[0]), int(ind_time_frag.split('P')[1])
+             else:
+                 ind_time = int(ind_time_frag)
+         elif filename.find('P') >=0:
+             plate_no = int(filename.split('P')[1])
+         
+         
          data.rename(columns={'Blank corrected based on Raw Data (600 1)': 'PR_Corrected OD600',
                               'Blank corrected based on Raw Data (584 2)': 'PR_Corrected Fluo (a.u.)',
                               'RF/OD600': 'PR_Corrected Red Fluo/OD600 (a.u.)'}, inplace=True)
          data['Run'] = run_no
          data['Post-induction (hrs)'] = ind_time
-         if ind_cond_code == '1':
-             data['Induction'] = '1 mM arabinose'
-         elif ind_cond_code == '2':
-             data['Induction'] = '1 mM arabinose + 0.1 mM caffeine'
-         else:
-             data['Induction'] = 'error'
+         data = data.merge(metadata_df,left_index=True,right_index=True)
          
-            # Append all metadata to dataframe with plate reader data
-         for meta_property, metadf_96format in metadata.items():
-             data = data.merge(metadf,left_index=True,right_index=True)
-        
-        # TODO: Check the row of blank that should be removed
-         # Remove data of the blank well 'H12'
-         data = data.drop('H12',axis=0)
+         # TODO: Check that blank info is in metadata excel file
+         blank_list = data.index[data['SampleID']=='Blank'].tolist()
+         data = data.drop(blank_list,axis=0)
+         
          data['PR_Well'] = data.index
          alldata = alldata.append(data,ignore_index=True,sort=None)
 
