c4f0bac20b31bc2e5f9001eab14cb24930d2323b
Trevor Y. H. Ho <trevor.ho@ed.ac.uk>
2019-08-01 20:45:48 +0100

    updated scripts for IBM_BM005 Screen1, also simplified some functions

--- IBM_ScreenDataProcessing.py
+++ IBM_ScreenDataProcessing.py
@@ -8,6 +8,7 @@ Created on Fri Feb 22 14:54:38 2019
 import os
 import pandas as pd
 import numpy as np
+import IBM_CustomFunctions as cf
 
 # Function for renaming Index
 def renameIndex(indexStr=str):
@@ -18,14 +19,13 @@ def renameIndex(indexStr=str):
     # all csv files that contain df data of fluorescence with inducer conc must be in this folder
     # this is also the folder where the output will be deposited
 dataRootDir = r'W:\Data storage & Projects\PhD Project_Trevor Ho\3_Intein-assisted Bisection Mapping'
-dataFolderDir = 'BM004'
-dataFolder2Dir = 'Screen5'
-outputCSV = 'BM004_Screen5_pooledData.csv'
+dataFolderDir = 'BM005'
+dataFolder2Dir = 'Screen1'
+outputCSV = 'BM005_Screen1_pooledData.csv'
 
 #%%
 # Create dict for handling multiple files at once
-filenameCoreList = {'PR_BM004_Screen5_PI5_': ['1','2'],
-                    'PR_BM004_Screen5_PI24_': ['1','2']
+filenameCoreList = {'PR_BM005_Screen1_PI18_': ['P1','P2']
                     }
 
 # Create list for induction OFF & ON states
@@ -33,9 +33,9 @@ inductionList = ['-','+']
 
 
 for fNVar, plateList in filenameCoreList.items():
-    mergedData = pd.DataFrame(columns=['Plate','Well','+C OD','+C Fluo'])
+    mergedData = pd.DataFrame(columns=[])
     for plateNo in plateList:
-        plateData = pd.DataFrame(columns=['Plate','Well','+C OD','+C Fluo'])
+        plateData = pd.DataFrame(columns=[])
         
         for indCon in inductionList:
             
@@ -60,11 +60,11 @@ for fNVar, plateList in filenameCoreList.items():
             plateData['Well'] = data['Well']
             
             if indCon == '-':
-                plateData['-C OD'] = data['Raw Data (600 1)']
-                plateData['-C Fluo'] = data[ 'Raw Data (584 2)']
+                plateData['-Ind OD'] = data['Raw Data (600 1)']
+                plateData['-Ind Fluo'] = data['Raw Data (584 2)']
             else:
-                plateData['+C OD'] = data['Raw Data (600 1)']
-                plateData['+C Fluo'] = data[ 'Raw Data (584 2)']
+                plateData['+Ind OD'] = data['Raw Data (600 1)']
+                plateData['+Ind Fluo'] = data['Raw Data (584 2)']
             
             # Update the index so it matches the conventional nomenclature
             indexList = plateData['Well'].tolist()
@@ -74,33 +74,16 @@ for fNVar, plateList in filenameCoreList.items():
             plateData['Plate'] = plateNo
             
             # Read Metadata Excelfile
-            metadataNameCore = 'BM004_Screen5_P' + plateNo + '_PRPlateMetadata.xlsx'
+            metadataNameCore = 'BM005_Screen1_' + plateNo + '_PRPlateMetadata.xlsx'
             metafilename = metadataNameCore
             metadataDir = os.path.join(dataRootDir,dataFolderDir,dataFolder2Dir,metafilename)
-            metaxls= pd.ExcelFile(metadataDir)
-            metadata = {sheet:metaxls.parse(sheet) for sheet in metaxls.sheet_names}    #import all sheets in metadata file into a dict, property name=keys, metadata df = values
-            # Process the df of metadata & merge into the main dataframe
-            for meta_property, metadf_96format in metadata.items():
-                metadf = pd.DataFrame(columns = [])
-                for char in ['A','B','C','D','E','F','G','H']:
-                    metadf_96_row = metadf_96format.transpose()[char]
-                    metadf_96_row = metadf_96_row.to_frame(meta_property)
-                    indexList = metadf_96_row.index.tolist()
-                    
-                    # Retrieve information of the wells and set it as index
-                    for columnIndex in range(len(indexList)):
-                        indexList[columnIndex] = char + str(indexList[columnIndex])
-                    metadf_96_row['Well']=indexList
-                    metadf_96_row.set_index('Well', inplace=True)
-                    
-                    # Append to major dataframe
-                    metadf = metadf.append(metadf_96_row,sort=False)
+            metadf = cf.metadata_to_metadf(metadataDir)
 
         plateData = plateData.merge(metadf,left_on='Well',right_index=True)
         mergedData = mergedData.append(plateData,ignore_index=False,sort=False)
-        mergedData['-C Fluo/OD'] =  mergedData['-C Fluo']/mergedData['-C OD']
-        mergedData['+C Fluo/OD'] =  mergedData['+C Fluo']/mergedData['+C OD']
-        mergedData['IndFoldChange'] = mergedData.apply(lambda mergedData :mergedData['+C Fluo/OD']/mergedData['-C Fluo/OD'] if mergedData['-C Fluo/OD']!=0 else np.nan,axis=1) 
+        mergedData['-Ind Fluo/OD'] =  mergedData['-Ind Fluo']/mergedData['-Ind OD']
+        mergedData['+Ind Fluo/OD'] =  mergedData['+Ind Fluo']/mergedData['+Ind OD']
+        mergedData['IndFoldChange'] = mergedData.apply(lambda mergedData :mergedData['-Ind Fluo/OD']/mergedData['+Ind Fluo/OD'] if mergedData['-Ind Fluo/OD']!=0 else np.nan,axis=1) 
 
     # Export Data File, one for each induction time point
     outputCSV = fNVar + 'pooledData.csv'
@@ -108,4 +91,3 @@ for fNVar, plateList in filenameCoreList.items():
     mergedData.to_csv(csvDir,header=True)
 
 #mergedData = mergedData.sort_values(by=['+C Fluo/OD'],ascending=False)
-    
