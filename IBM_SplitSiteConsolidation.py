e6ab137d2f77429c04e5d7fa3715f5c9e6d6ea62
Trevor Y. H. Ho <trevor.ho@ed.ac.uk>
2019-08-29 18:48:04 +0100

    Multiple Updates for BM005 & FC023 1. Renamed variables for clarity 2. Automated horizontal lines plotting 3. Created extra panel for plotting control fluos 4. Better handling of ticks 5. Moved legends outside of the graph 6. Automated error bars plotting 7. Split site arrows removed in this version. Need to restored and reworked for other BM

--- IBM_SplitSiteConsolidation.py
+++ IBM_SplitSiteConsolidation.py
@@ -12,7 +12,9 @@ import os
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
-from matplotlib.ticker import AutoMinorLocator
+from matplotlib.ticker import FixedLocator, AutoMinorLocator
+from matplotlib import gridspec
+import numpy as np
 
 sns.reset_defaults
 
@@ -22,30 +24,77 @@ dataRootDir = r'W:\Data storage & Projects\PhD Project_Trevor Ho\3_Intein-assist
 # TODO: Specify location of CSV file containing identified split sites
 ssFolderDir1 = 'BM005'
 ssFolderDir2 = 'BM005_Sequencing Results'
-ss_csv = 'IBM_BM005_IdentifiedSplitSites.csv'
+ssDataFN = 'IBM_BM005_IdentifiedSplitSites.csv'
 
 # TODO: Specify location of CSV file containing the median fluorescence values of the strains
 fluoFolderDir='FC023'
-fluo_csv = 'IBM_FC023R3-5_FCmedian&metadata&PRData_InductionProcessed.csv'
+fluoDataFN = 'IBM_FC023R3-5_FCmedian&metadata&PRData_InductionProcessed_FoldChangeFiltered.csv'
+
 # TODO: Specify location of CSV file containing the median fluorescence values of control strains
-control_fluo_csv = 'IBM_FC023R3-5_FCmedianStats&metadata.csv'
+ctrlFluoDataFN = 'IBM_FC023R3-5_FCmedianStats&metadata_FoldChangeFiltered.csv'
 
 # TODO: Specify filename to be saved. Figure will be saved under path of dataRootDir\ssFolderDir1
-figName = 'IBM_BM005_BisectionMap_Raw.eps'
+figName = 'IBM_BM005_BisectionMap_Raw.pdf'
 
+# TODO: Specify filename of csv with merged data to be saved (final data saves under BM005 directly)
+mergedDataFN = 'IBM_BM005_PooledResults.csv'
+
+
+# Preparations
+
+# TODO: Set induction and induction time information
+inductions = ['no induction','1 mM arabinose','25 μM DAPG','1 mM arabinose + 25 μM DAPG']
+pi_times = [5,24]
+
+# TODO: Update control list
+control_list = ['IBMc252 + IBMc249',
+        'IBMc259 + IBMc249',
+        'IBMc259 + IBMc226']
+
+# TODO: Set x tick labels of controls
+ctrl_tick_labels = ['background','reporter','TetR + reporter']
+
+# TODO: Input information for constructing hlines
+# hline_input instruction:
+    # key = str for df.query. Note that str inside str are double-quoted
+    # value = list of colors to use, 1st color = line color, 2nd color = shade color
+hline_input = {
+        'SampleID == "IBMc252 + IBMc249"': ['k','#E8E8E8'],
+        'SampleID == "IBMc259 + IBMc249"': ['#F96495','#eda3ba'],
+        'SampleID == "IBMc259 + IBMc226" & (Induction == "no induction" | Induction == "25 μM DAPG")': ['#A952A5','#E9D4E9'],
+        'SampleID == "IBMc259 + IBMc226" & (Induction == "1 mM arabinose" | Induction == "1 mM arabinose + 25 μM DAPG")': ['#A952A5','#E9D4E9'],
+        }
+
+# TODO: Define how many amino acids are being plotted
+start_aa = 1
+end_aa = 207
+
+# TODO: Input transposition window
+n_trans_border = 21
+c_trans_border = 188
+
+# TODO: Decide color for inductions
+color_mapper = {
+        'no induction':'#F96495',
+        '1 mM arabinose':'#55A0FB',
+        '25 μM DAPG':'#099963',
+        '1 mM arabinose + 25 μM DAPG':'#1D1D1B'
+        }
+
+#%%
 '''Read data and map split sites to function'''
 # Read split site csv file
-ss_csvDir = os.path.join(dataRootDir,ssFolderDir1,ssFolderDir2,ss_csv)
-ss_data = pd.read_csv(ss_csvDir,index_col=0)
-ss_data = ss_data[['aa_ss_middle']]
-ss_data['SampleID']=ss_data.index
+ssDataDir = os.path.join(dataRootDir,ssFolderDir1,ssFolderDir2,ssDataFN)
+ssData = pd.read_csv(ssDataDir,index_col=0)
+ssData = ssData[['aa_ss_middle']]
+ssData['SampleID']=ssData.index
 
 # Read fluorescence csv file
-fluo_csvDir = os.path.join(dataRootDir,fluoFolderDir,fluo_csv)
-fluo_data = pd.read_csv(fluo_csvDir,index_col=0)
+fluoDataDir = os.path.join(dataRootDir,fluoFolderDir,fluoDataFN)
+fluoData = pd.read_csv(fluoDataDir,index_col=0)
 
 # Map split site to fluorescence
-mergedData = fluo_data.merge(ss_data,sort=False)
+mergedData = fluoData.merge(ssData,sort=False)
 # Generate a stripped mergedData set for histogram plot
 mergedDataSingleCon=mergedData[(mergedData['Run']==3) \
                                & (mergedData['Induction']=='1 mM arabinose') \
@@ -53,12 +102,9 @@ mergedDataSingleCon=mergedData[(mergedData['Run']==3) \
 mergedDataSingleCon=mergedDataSingleCon['aa_ss_middle']
 
 '''Loop through the amino acid split sites and consolidate median fluorescence'''
-# Preparations
-inductions = ['no induction','1 mM arabinose','25 μM DAPG','1 mM arabinose + 25 μM DAPG']
-pi_times = [5,24]
 
 # Deduplicate the list of amino acid split sites and sort to a list
-unique_ss_list = ss_data['aa_ss_middle'].drop_duplicates().sort_values().tolist()
+unique_ss_list = ssData['aa_ss_middle'].drop_duplicates().sort_values().tolist()
 
 unique_ss_data = pd.DataFrame(columns=[])
 for pi_time in pi_times:
@@ -79,154 +125,202 @@ for pi_time in pi_times:
             unique_ss_data = unique_ss_data.append(rowInfo,sort=False)
 
 # Export the data to keep it somewhere safe
+mergedDataDir = os.path.join(dataRootDir,ssFolderDir1,mergedDataFN)
+mergedData.to_csv(mergedDataDir)
+            
+#%% Calculate stats for hlines
+pi_dict = {1:5, 2:24}
+hline_data = pd.DataFrame(columns=[])
+
+for _, pi_time in pi_dict.items():
+    for queryLine, color in hline_input.items():
+        hRefSubData = fluoData.query(queryLine)
+        hRefSubData = hRefSubData[hRefSubData['Post-induction (hrs)']==pi_time]
+        hRefSubData = hRefSubData['median fluorescence (a.u.)']        
+        hline_data_row_dict = {
+                'Post-induction (hrs)': [pi_time],
+                'mean of median fluorescence (a.u.)': [hRefSubData.mean()],
+                'std of median fluorescence (a.u.)': [hRefSubData.std()],
+                'span for std': [(hRefSubData.std() > hRefSubData.mean()*0.1)],
+                'line color': [color[0]],
+                'shade color': [color[1]]
+                }
+        hline_data = hline_data.append(pd.DataFrame.from_dict(hline_data_row_dict))
 
 
+# Import CSV containing control data for plotting
+ctrlFluoDataDir = os.path.join(dataRootDir,fluoFolderDir,ctrlFluoDataFN)
+ctrlFluoData = pd.read_csv(ctrlFluoDataDir,index_col=0)
+ctrlFluoData = ctrlFluoData[ctrlFluoData['SampleID'].isin(control_list)]
+ctrlFluoData['SortIndex'] = ctrlFluoData.apply(lambda data: control_list.index(data['SampleID'])+1, axis=1)
 #%%
-
-'''Plot the graphs'''
-
-# TODO: Define how many amino acids are being plotted
-start_aa = 1
-end_aa = 200
-
-# Import CSV containing control data
-control_fluo_csvDir = os.path.join(dataRootDir,fluoFolderDir,control_fluo_csv)
-control_fluo_data = pd.read_csv(control_fluo_csvDir,index_col=0)
-
-# TODO: Check control data to be used
-# Keep control data only
-control_list = ['IBMc252 + IBMc249',
-    'IBMc259 + IBMc249']
-control_fluo_data = control_fluo_data[control_fluo_data['SampleID'].isin(control_list)]
-
 # Create canvas
 sns.set(style='ticks')
-fig, ax = plt.subplots(3, 1, sharex=True, figsize=(6, 8), dpi = 100)
+fig, ax = plt.subplots(3, 2, sharex=True, sharey=True, figsize=(6, 6), dpi = 100)
+# Grid spec
+ctrl_len = len(control_list)
+gs = gridspec.GridSpec(3, 2, width_ratios=[ctrl_len+10, end_aa]) 
+
+# Delete top-left plot
+fig.delaxes(ax[0,0])
 
 # Plot the counts of strains being mapped to the same split site
-sns.distplot(mergedDataSingleCon, kde=False, color="k", ax=ax[0], bins=end_aa)
-ax[0].set_xlabel('')
-ax[0].set_ylabel('sequencing \n count')
-ax[0].tick_params(axis='y', which='minor', left=True)
+ax[0,1] = plt.subplot(gs[0,1])
+sns.distplot(mergedDataSingleCon, kde=False, color="k", ax=ax[0,1], bins=end_aa)
+ax[0,1].set_xlabel('')
+ax[0,1].set_ylabel('sequencing \n count')
+ax[0,1].tick_params(axis='y', which='minor', left=True)
 minorLocator = AutoMinorLocator(5)
-ax[0].yaxis.set_minor_locator(minorLocator)
+ax[0,1].yaxis.set_minor_locator(minorLocator)
+ax[0,1].axvline(n_trans_border, color='k', dashes=[2,2], linestyle = '--', zorder=5)
+ax[0,1].axvline(c_trans_border, color='k', dashes=[2,2], linestyle = '--', zorder=5)
+ax[0,1].set_xlim(start_aa,end_aa)
+ax[0,1].set_xticklabels('')
 
-# Plot fluorescence of split sites
+# Plot the graphs
 
-pi_dict = {1:5, 2:24}
-for ax_ID, pi_time in pi_dict.items():
-    
-    # Create df to isolate data of interest
-    subdata = unique_ss_data[unique_ss_data['Post-induction (hrs)']==pi_time]
-    
-    sns.scatterplot(x='position (aa)', y='mean of median fluorescence (a.u.)', 
-                       hue_order=['no induction','1 mM arabinose','25 μM DAPG','1 mM arabinose + 25 μM DAPG'],
-                       hue='Induction',
-                       #palette=['#808080','r'],
-                       data=subdata, ax=ax[ax_ID],zorder=10,
-                       edgecolor=None)
-    ax[ax_ID].set(yscale="log")
+for col_ax_ID in [0,1]:
+    for row_ax_ID, pi_time in pi_dict.items():
+        
+        if col_ax_ID ==0:
+            # Create df to isolate data of interest
+            subdata = ctrlFluoData[ctrlFluoData['Post-induction (hrs)']==pi_time]
+            xaxis = 'SortIndex'
+        elif col_ax_ID == 1:
+            # Create df to isolate data of split sites for each time point
+            subdata = unique_ss_data[unique_ss_data['Post-induction (hrs)']==pi_time]
+            xaxis = 'position (aa)'
+        
+        ax[row_ax_ID,col_ax_ID] = plt.subplot(gs[row_ax_ID,col_ax_ID])
+        sns.scatterplot(x=xaxis, y='mean of median fluorescence (a.u.)', 
+                           hue='Induction',
+                           hue_order=inductions,
+                           palette=color_mapper,
+                           data=subdata, ax=ax[row_ax_ID,col_ax_ID],zorder=10,
+                           size = 3,
+                           edgecolor=None)
+            
+        # Plot error bars
+        for induction in inductions:
+            subdata_ind = subdata[subdata['Induction']==induction]
+            xVec = subdata_ind[xaxis].tolist()
+            meanVec = subdata_ind['mean of median fluorescence (a.u.)'].tolist()
+            errVec = subdata_ind['std of median fluorescence (a.u.)'].tolist()
+            ax[row_ax_ID,1].errorbar(x=xVec, y=meanVec, yerr = errVec, fmt='none',
+              zorder=6, color=color_mapper[induction]
+              )
 
-    # Plot error bars
-    errbar_colors = {'1 mM arabinose': '#808080',
-                     '1 mM arabinose + 0.1 mM caffeine': 'r'}
-    for induction in inductions:
-        sub_unique_ss_data = subdata[subdata['Induction']==induction]
-        ssVec = sub_unique_ss_data['position (aa)'].tolist()
-        meanVec = sub_unique_ss_data['mean of median fluorescence (a.u.)'].tolist()
-        errVec = sub_unique_ss_data['std of median fluorescence (a.u.)'].tolist()
-        ax[ax_ID].errorbar(x=ssVec, y=meanVec, yerr = errVec, fmt='none',
-          zorder=6
-#          , color=errbar_colors[induction]
-          )
+        # Remove legend from plot
+        ax[row_ax_ID,col_ax_ID].get_legend().remove() 
+                
+        # TODO: Check range of fluorescence
+        ax[row_ax_ID,col_ax_ID].set(yscale="log")
+        ax[row_ax_ID,col_ax_ID].set_ylim(1e2, 3e5)
+        
+        # Plot horizontal lines
+        hline_data_to_plot = hline_data[hline_data['Post-induction (hrs)'] == pi_time]
+        
+        for hline_data_to_plot_row in hline_data_to_plot.itertuples():
+            hline_mean = hline_data_to_plot_row[2]
+            hline_sd = hline_data_to_plot_row[3]
+            hline_color = hline_data_to_plot_row[5]
+            hline_shade = hline_data_to_plot_row[6]
+            
+#            if hline_data_to_plot_row[4] == True:
+#                ax[ax_ID,1].axhspan(hline_mean - hline_sd, hline_mean + hline_sd, facecolor=hline_shade, zorder=0)
+#            else:
+#            ax[row_ax_ID,1].axhline(hline_mean, color=hline_shade, zorder=0)
+            ax[row_ax_ID,col_ax_ID].axhline(hline_mean, color=hline_color, dashes=[2,2], linestyle = '--', zorder=0)    
+        
+    #    # Annotate split sites to match 3D structures
+    #    ss_color_list = ['#f9897a',
+    #                     '#f9a4ed',
+    #                     '#f9a4ed',
+    #                     '#ffcd70',
+    #                     '#7ffaff',
+    #                     '#f2f90e',
+    #                     '#91a9ea',
+    #                     '#025ff4',
+    #                     '#025ff4',
+    #                     '#77fc5f',
+    #                     '#77fc5f',
+    #                     '#77fc5f'               
+    #            ]
+    #    ss_color_map = dict(zip(unique_ss_list,list(zip(meanVec,ss_color_list))))
+    #    for ss, color_info in ss_color_map.items():
+    ##    ax[3].axvline(ss, color=color, linestyle = '-',zorder=0)
+    #        ax[ax_ID].annotate('',xy=(ss+0.25, color_info[0]*1.2), xytext=(ss+1.5, color_info[0]*2),
+    #              zorder=3,
+    #              arrowprops=dict(facecolor=color_info[1], width=5, headwidth=8, **{'edgecolor':'None'}))
     
-    # TODO: Check range of identified split sites & range of fluorescence
-    ax[ax_ID].set_ylim(1e0, 6e5)
-    ax[ax_ID].set_xlim(start_aa,end_aa)
 
-    # TODO: Check transposition window location
-    n_trans_border = 6
-    ax[ax_ID].axvline(n_trans_border, color='k', dashes=[2,2], linestyle = '--', zorder=5)
-    # also add line for count plot
-    ax[0].axvline(n_trans_border, color='k', dashes=[2,2], linestyle = '--', zorder=5)
-    
-    # Plot minor ticks on x axis (optional depending on graph)
-    minorLocator = AutoMinorLocator(5)
-    ax[ax_ID].xaxis.set_minor_locator(minorLocator)
+# Extra customizations to control fluorescence plots
+for row_ax_ID in [1,2]:
+    ax[row_ax_ID,0].set_xlim(0, ctrl_len+1)
+    ax[row_ax_ID,0].set_xticks(range(1,ctrl_len+1))
+    ax[row_ax_ID,0].set_xticklabels('')
+    ax[row_ax_ID,0].set_xlabel('')
 
-    # Calculate values of control data for horizontal line plotting
-    sub_control_data = control_fluo_data[control_fluo_data['Post-induction (hrs)']==pi_time]
-    negdata = sub_control_data[(sub_control_data['SampleID']==control_list[0])]
-    negFluos = negdata['mean of median fluorescence (a.u.)']
-    neg_mean = negFluos.mean()
-    neg_sd = negFluos.std()
-    
-    posdata = sub_control_data[sub_control_data['SampleID']==control_list[1]]
-    posFluos = posdata['mean of median fluorescence (a.u.)']
-    pos_mean = posFluos.mean()
-    pos_sd = posFluos.std()
-    
-    neg_sd_span = True
-    if neg_sd < neg_mean*0.05:
-        neg_sd_span = False
-    
-    pos_sd_span = True
-    if pos_sd < pos_mean*0.05:
-        pos_sd_span = False
-    
-    # Plot negative control
-    if neg_sd_span == True:
-        ax[ax_ID].axhspan(neg_mean - neg_sd, neg_mean + neg_sd, facecolor='#E8E8E8', zorder=0)
-    else:
-        ax[ax_ID].axhline(neg_mean, color='#E8E8E8', zorder=0)
-    ax[ax_ID].axhline(neg_mean, color='k', dashes=[2,2], linestyle = '--', zorder=0)
-    
-    # Plot positive control
-    if pos_sd_span == True:
-        ax[ax_ID].axhspan(pos_mean - pos_sd, pos_mean + pos_sd, facecolor='#eda3ba', zorder=0)
-    else:
-        ax[ax_ID].axhline(pos_mean, color='#eda3ba', zorder=0)
-    ax[ax_ID].axhline(pos_mean, color='r', dashes=[2,2], linestyle = '--', zorder=0)
+# Extra customizations to BM fluorescence plots
+for row_ax_ID in [1,2]:
+    ax[row_ax_ID,1].set_xlim(start_aa,end_aa)
+    ax[row_ax_ID,1].set_xlabel('')
+    ax[row_ax_ID,1].set_yticklabels('')
+    ax[row_ax_ID,1].set_ylabel('')
+ax[1,1].set_xticklabels('')
 
-    # Remove legend from plot
-    ax[ax_ID].get_legend().remove()
-    
-#    # Annotate split sites to match 3D structures
-#    ss_color_list = ['#f9897a',
-#                     '#f9a4ed',
-#                     '#f9a4ed',
-#                     '#ffcd70',
-#                     '#7ffaff',
-#                     '#f2f90e',
-#                     '#91a9ea',
-#                     '#025ff4',
-#                     '#025ff4',
-#                     '#77fc5f',
-#                     '#77fc5f',
-#                     '#77fc5f'               
-#            ]
-#    ss_color_map = dict(zip(unique_ss_list,list(zip(meanVec,ss_color_list))))
-#    for ss, color_info in ss_color_map.items():
-##    ax[3].axvline(ss, color=color, linestyle = '-',zorder=0)
-#        ax[ax_ID].annotate('',xy=(ss+0.25, color_info[0]*1.2), xytext=(ss+1.5, color_info[0]*2),
-#              zorder=3,
-#              arrowprops=dict(facecolor=color_info[1], width=5, headwidth=8, **{'edgecolor':'None'}))
-    
-# Customize y labels of the two different graphs
-ax[1].set_ylabel('5 hours')
-ax[2].set_ylabel('24 hours')
-
-# Customize legend for fluorescence
+# TODO: Customize legend for fluorescence
 # frameon=False removes the frame where the labels were in
 # Get the hue category label and removes it    
-handles, labels = ax[1].get_legend_handles_labels()
-ax[1].legend(handles=handles[1:], labels=labels[1:],
-  loc='upper right',frameon=False)
+handles, labels = ax[2,1].get_legend_handles_labels()
+labels = [
+        '',
+        'no induction',
+        '1 mM arabinose (N-lobe)',
+        '25 μM DAPG (C-lobe)',
+        '1 mM arabinose + 25 μM DAPG (N+C lobes)'
+        ]
+ax[2,1].legend(
+        handles=handles[1:], labels=labels[1:],
+      bbox_to_anchor=(1.005, 1),frameon=False, ncol=1,
+      prop={'size': 12})
 
-#%%
+# Draw transposition window for all plots on the 2nd column
+for ax_ID in [0,1,2]:
+    ax[ax_ID,1].axvline(n_trans_border, color='k', dashes=[2,2], linestyle = '--', zorder=5)
+    ax[ax_ID,1].axvline(c_trans_border, color='k', dashes=[2,2], linestyle = '--', zorder=5)  
+
+# Customize outer labels
+ax[1,0].set_ylabel('5 hours')
+ax[2,0].set_ylabel('24 hours')
+ax[2,1].set_xlabel('position (aa)')
+ax[2,0].set_xticklabels(ctrl_tick_labels, rotation=40, fontsize=10, ha="right")
+
+
+# TODO: Extra x ticks
+
+minorLocator = AutoMinorLocator(10)
+
+for row_ax_ID in [0,1,2]:
+    
+    xticks = ax[row_ax_ID,1].get_xticks() 
+    xticks = np.append(xticks,1)
+    xticks = np.append(xticks,207)
+    
+    xtickslist=xticks.tolist()
+    xtickslist[-2]="1"
+    xtickslist[-1]="207"
+    ax[row_ax_ID,1].set_xticks(xticks)
+    ax[2,1].set_xticklabels(xtickslist)
+    
+    # Plot minor ticks on x axis (optional depending on graph)
+    ax[row_ax_ID,1].xaxis.set_minor_locator(minorLocator)
+    
+    ax[row_ax_ID,1].set_xlim(start_aa,end_aa)
+
 # Make room for plot at the bottom
-plt.gcf().subplots_adjust(bottom=0.2)
+plt.gcf().subplots_adjust(hspace=0.15, wspace=0.1)
 
 '''Save Figure'''
 outputDir = os.path.join(dataRootDir,ssFolderDir1,figName)
-fig.savefig(outputDir)
+#fig.savefig(outputDir)
