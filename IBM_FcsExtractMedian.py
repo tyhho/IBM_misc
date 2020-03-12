8af083a7fd693aa3ac1262fdb1d943a20f720d93
Trevor Y. H. Ho <trevor.ho@ed.ac.uk>
2020-03-12 21:40:01 +0000

    IBM_FC032R2 completed

--- IBM_FcsExtractMedian.py
+++ IBM_FcsExtractMedian.py
@@ -25,16 +25,16 @@ dataFolderDir = 'FC032'
 #pr_data_filename = 'IBM_FC021R2-7_Finalized_PRData.csv'
 
 # TODO: Specify the output filename for the combined data
-all_doi_filename = 'IBM_FC032R1_median&metadata&PRData.csv'
+all_doi_filename = 'IBM_FC032R2_median&metadata&PRData.csv'
 
 # TODO: Specify metadata file core
-metadatafnCore = 'PRMD_IBM_FC032'
+metadatafnCore = 'PRMD_IBM_FC032R2'
 
 #%%
 # TODO: Specify folder sequence for processing
 
 coreSearchSeqList = [
-            'IBM_FC032R1PI*_FCS',
+            'IBM_FC032R2PI*_FCS',
 #            'IBM_FC021R[2,3,4]*P2_FCS',
 #            'IBM_FC021R[2-7]*_FCS'
             ]
--- IBM_PRDataProcessing.py
+++ IBM_PRDataProcessing.py
@@ -19,8 +19,8 @@ import fnmatch
 
 # TODO: Specify folder location
 dataRootDir=r'W:\Data storage & Projects\PhD Project_Trevor Ho\3_Intein-assisted Bisection Mapping'
-dataFolderDir='EXP012'
-outputCSV = 'IBM_EXP012_PRData.csv'
+dataFolderDir='FC032'
+outputCSV = 'IBM_FC032_PRData.csv'
 
 # TODO: Prepare file of metadata
 # Check that the blank well has been labeled as "Blank"
@@ -44,7 +44,7 @@ allFiles = os.listdir(folderDir)
             # if 'filename' here is given as 'customFunction', then it will call the findBlankXlsx function to look for the blank
         # Having a blank info will force the script to use the blank given instead of the blank in metadata file
     
-fileList = {'PR_IBM_EXP012R1*.xlsx': ['PRMD_IBM_EXP012.xlsx']
+fileList = {'PR_IBM_FC032R2*.xlsx': ['PRMD_IBM_FC032R2.xlsx']
             }
 
 
@@ -119,7 +119,7 @@ for fnSearchSeq, metaInfo in fileList.items():
                                  'Raw Data (584 2)': 'PR_Corrected Red Fluorescence (a.u.)'}, inplace=True)
             # Drop any possible remaining blank data
             if from_meta_blank_list:
-                data = data.drop(from_meta_blank_list,axis=0)
+                data = data.drop(from_meta_blank_listk_list,axis=0)
         
         # Add extra metadata based on info in filename
         # Extract information from filename
@@ -139,11 +139,11 @@ for fnSearchSeq, metaInfo in fileList.items():
             except ValueError:
                 ind_time = ind_time_frag.split('P')[0]
         elif fnInfo.find('P') >=0:
-            pass
-            # plate_no = int(fnInfo.split('P')[1])
+            # pass
+            plate_no = int(fnInfo.split('P')[1])
         
         data['Run'] = run_no
-        # data['Post-induction (hrs)'] = ind_time
+        data['Post-induction (hrs)'] = ind_time
         
         try:
             data['PR_Plate'] = plate_no
